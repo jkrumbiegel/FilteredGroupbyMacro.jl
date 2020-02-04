f48c1d03c7eda205a07f185715a96e5c506e6a5b
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-02-04 12:53:53 +0100

    first real version with a few tests

--- src/GroupbyIndexingMacro.jl
+++ src/GroupbyIndexingMacro.jl
@@ -1,5 +1,94 @@
 module GroupbyIndexingMacro
 
-greet() = print("Hello World!")
+import DataFrames
+
+export @dt
+
+macro dt(exp)
+    # dump(exp)
+
+    exp.head != :ref && error("You have to use [] syntax.")
+
+    df = exp.args[1]
+
+    # enable chained calls by applying the macro again if df is actually another nested
+    # ref expression
+    if df isa Expr
+        df = :(@dt($df))
+    end
+
+
+    # nargs = length(exp.args) - 1
+    args = exp.args[2:end]
+
+    filterexp = args[1]
+    byexp = args[2]
+    kwexps = args[3:end]
+
+    conv_filterexp = replace_quotenodes_filterexp!(filterexp, df)
+    conv_kwexps = convert_kwexp.(kwexps)
+
+    quote
+        $(esc(DataFrames.by))(
+            # filter
+            $(esc(df))[$(esc(conv_filterexp)), $(esc(:))],
+            # groupby
+            $byexp;
+            # new columns from computations
+            $(conv_kwexps...))
+    end
+end
+
+
+function convert_kwexp(kwexp)
+    @assert kwexp.head == :kw
+    kw = kwexp.args[1]
+    exp = kwexp.args[2]
+
+    replace_quotenodes!(exp)
+    quotenodes = all_quotenodes(exp)
+
+    :($(esc(kw)) = ($(quotenodes...),) => $(esc(:subdf)) -> $exp)
+end
+
+function all_quotenodes(exp)
+    quotenodes = QuoteNode[]
+    for a in exp.args
+        if a isa QuoteNode
+            push!(quotenodes, a)
+        elseif a isa Expr
+            append!(quotenodes, all_quotenodes(a))
+        end
+    end
+    quotenodes
+end
+
+replace_quotenodes!(any) = any
+
+function replace_quotenodes!(exp::Expr)
+    for (i, a) in enumerate(exp.args)
+        if a isa QuoteNode
+            sym = a.value
+            exp.args[i] = :($(esc(:subdf)).$sym)
+        elseif a isa Expr
+            replace_quotenodes!(a)
+        end
+    end
+    exp
+end
+
+replace_quotenodes_filterexp!(any, dfexp) = any
+
+function replace_quotenodes_filterexp!(exp::Expr, dfexp)
+    for (i, a) in enumerate(exp.args)
+        if a isa QuoteNode
+            sym = a.value
+            exp.args[i] = :($dfexp.$sym)
+        elseif a isa Expr
+            replace_quotenodes_filterexp!(a, dfexp)
+        end
+    end
+    exp
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,29 @@
 using GroupbyIndexingMacro
+using DataFrames
 using Test
 
 @testset "GroupbyIndexingMacro.jl" begin
-    # Write your own tests here.
+    df = DataFrame(
+        a = repeat(1:10, inner = 10),
+        b = repeat('a':'e', inner = 20),
+        c = 1:100,
+    )
+
+    dt_1 = @dt df[!, :b, d = :a .* :c]
+    by_1 = by(df, :b, d = (:a, :c) => x -> x.a .* x.c)
+
+    # do both versions do the same thing?
+    @test dt_1 == by_1
+
+    # test chaining of multiple groupings
+    dt_2 = @dt dt_1[!, :b, e = sum(:d .^ 2)]
+    @test dt_2 == @dt df[!, :b, d = :a .* :c][
+        !, :b, e = sum(:d .^ 2)]
+
+
+    # test filtering
+    dt_3 = @dt df[:a .< 8, :b, d = sum(:c)]
+    by_3 = by(df[df.a .< 8, :], :b, d = (:c,) => x -> sum(x.c))
+    @test dt_3 == by_3
 end
+
